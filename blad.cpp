f1572c4a30ed5ac9b7f4ca6e765659de264c706e
kudry-vlad <kudry-vlad523@mail.ru>
2014-11-18 17:07:36 +0400

    Create blad.cpp

---
+++ blad.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+	#include <iostream>
+	#include <locale.h>
+	using namespace std; 
+
+	int main(void) {
+		setlocale(LC_ALL, "Russia");
+		int h1, m1, h2, m2;
+		cout << "Введите время отправления (hh mm): ";
+		cin >> h1 >> m1;
+		cout << "Введите время в пути (hh mm): ";
+		cin >> h2 >> m2;
+		int d3, h3, m3;
+		m3 = (m1 + m2) % 60;
+		int hext = (m1 + m2) / 60;
+		h3 = (h1 + h2 + hext) % 24;
+		d3 = 1 + (h1 + h2 + hext) / 24;
+		cout << "Время прибытия: день " << d3 << ", " << h3 << ":" << m3 << endl;
+		return 0;
+		}
